90677801d0178b67fe6c823ee99d612af1450686
Nichole Bates <nbates88@gmail.com>
2016-08-09 00:33:39 -0400

    commit

---
+++ app/scripts/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myNewProjectApp
+ * @description
+ * # myNewProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myNewProjectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'angular.filter'
+  ])
+  .config(function ($stateProvider) {
+    
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .state('about', {
+        url:'/about',
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'views/projects.html',
+      })
+      .state('project', {
+        url: '/project/:id',
+       templateUrl: 'views/project.html',
+      });
+     
+  })
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myNewProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myNewProjectApp
+ */
+angular.module('myNewProjectApp')
+  .controller('MainCtrl', function () {
+  	
+
+  });
