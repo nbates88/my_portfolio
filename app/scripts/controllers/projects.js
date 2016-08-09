f1cd326284e37e7f49a250340c35f74a83eee927
Nichole Bates <nbates88@gmail.com>
2016-08-09 01:02:41 -0400

    more changes

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -1,4 +1,11 @@
 'use strict';
+/**
+ * @ngdoc function
+ * @name myNewProjectApp.controller:ProjectsCtrl
+ * @description
+ * # ProjectsCtrl
+ * Controller of the myNewProjectApp
+ */
 angular.module('myNewProjectApp')
   .controller('ProjectsCtrl', function () {
   	
